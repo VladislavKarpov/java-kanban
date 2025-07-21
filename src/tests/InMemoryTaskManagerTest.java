3fa23244c22e11212c3914fb77beb2724c8c4a84
badsoul19 <treeworld05@gmail.com>
2025-07-21 15:16:19 +0300

    Исправить расположение папки tests

--- src/tests/InMemoryTaskManagerTest.java
+++ src/tests/InMemoryTaskManagerTest.java
@@ -1,4 +1,4 @@
-package test;
+package tests;
 
 import manager.InMemoryTaskManager;
 import manager.Managers;
--- src/tests/ManagersTest.java
+++ src/tests/ManagersTest.java
@@ -1,4 +1,4 @@
-package test;
+package tests;
 
 import manager.*;
 import org.junit.jupiter.api.Test;
